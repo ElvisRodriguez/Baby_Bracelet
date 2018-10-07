a219fa9234e6a54be70a3b5eaac3acfe7ebe47f5
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-07 22:54:21 +0000

    - Modified app.py

--- BabyBraceUIDemo/app.py
+++ BabyBraceUIDemo/app.py
@@ -59,4 +59,4 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host = '127.0.0.1', port=5000)
+    app.run_server(debug=True, host='0.0.0.0')
