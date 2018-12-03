05877b005d75e20584a09a7b84f300edf767795d
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2018-12-03 17:16:14 +0000

    ported heartBeats to py3

--- QA/heartBeats.py
+++ QA/heartBeats.py
@@ -1,5 +1,5 @@
 # Simple heart beat reader for Raspberry pi using ADS1x15 family of ADCs and a pulse sensor - http://pulsesensor.com/.
-# The code borrows heavily from Tony DiCola's examples of using ADS1x15 with 
+# The code borrows heavily from Tony DiCola's examples of using ADS1x15 with
 # Raspberry pi and WorldFamousElectronics's code for PulseSensor_Amped_Arduino
 
 # Author: Udayan Kumar
@@ -13,8 +13,8 @@ import Adafruit_ADS1x15
 if __name__ == '__main__':
 
     adc = Adafruit_ADS1x15.ADS1015()
-    # initialization 
-    GAIN = 2/3  
+    # initialization
+    GAIN = 2/3
     curState = 0
     thresh = 525  # mid point in the waveform
     P = 512
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         ##  find the peak and trough of the pulse wave
         if Signal < thresh and N > (IBI/5.0)*3.0 :  #       # avoid dichrotic noise by waiting 3/5 of last IBI
             if Signal < T :                        # T is the trough
-              T = Signal;                         # keep track of lowest point in pulse wave 
+              T = Signal;                         # keep track of lowest point in pulse wave
 
         if Signal > thresh and  Signal > P:           # thresh condition helps avoid noise
             P = Signal;                             # P is the peak
@@ -54,7 +54,7 @@ if __name__ == '__main__':
           #  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
           # signal surges up in value every time there is a pulse
         if N > 250 :                                   # avoid high frequency noise
-            if  (Signal > thresh) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :       
+            if  (Signal > thresh) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :
               Pulse = True;                               # set the Pulse flag when we think there is a pulse
               IBI = sampleCounter - lastBeatTime;         # measure time between beats in mS
               lastBeatTime = sampleCounter;               # keep track of time for next pulse
@@ -62,7 +62,7 @@ if __name__ == '__main__':
               if secondBeat :                        # if this is the second beat, if secondBeat == TRUE
                 secondBeat = False;                  # clear secondBeat flag
                 for i in range(0,10):             # seed the running total to get a realisitic BPM at startup
-                  rate[i] = IBI;                      
+                  rate[i] = IBI;
 
               if firstBeat :                        # if it's the first time we found a beat, if firstBeat == TRUE
                 firstBeat = False;                   # clear firstBeat flag
@@ -71,17 +71,17 @@ if __name__ == '__main__':
 
 
               # keep a running total of the last 10 IBI values
-              runningTotal = 0;                  # clear the runningTotal variable    
+              runningTotal = 0;                  # clear the runningTotal variable
 
               for i in range(0,9):                # shift data in the rate array
-                rate[i] = rate[i+1];                  # and drop the oldest IBI value 
+                rate[i] = rate[i+1];                  # and drop the oldest IBI value
                 runningTotal += rate[i];              # add up the 9 oldest IBI values
 
               rate[9] = IBI;                          # add the latest IBI to the rate array
               runningTotal += rate[9];                # add the latest IBI to runningTotal
-              runningTotal /= 10;                     # average the last 10 IBI values 
+              runningTotal /= 10;                     # average the last 10 IBI values
               BPM = 60000/runningTotal;               # how many beats can fit into a minute? that's BPM!
-              print 'BPM: {}'.format(BPM)
+              print('BPM: {}'.format(BPM))
 
         if Signal < thresh and Pulse == True :   # when the values are going down, the beat is over
             Pulse = False;                         # reset the Pulse flag so we can do it again
@@ -94,10 +94,9 @@ if __name__ == '__main__':
             thresh = 512;                          # set thresh default
             P = 512;                               # set P default
             T = 512;                               # set T default
-            lastBeatTime = sampleCounter;          # bring the lastBeatTime up to date        
+            lastBeatTime = sampleCounter;          # bring the lastBeatTime up to date
             firstBeat = True;                      # set these to avoid noise
             secondBeat = False;                    # when we get the heartbeat back
-            print "no beats found"
+            print("no beats found")
 
         time.sleep(0.005)
-
