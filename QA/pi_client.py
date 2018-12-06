456e408da4220b224cdfe7562dcd6e8f0139161f
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2018-12-06 17:27:10 +0000

    Testing Socket Connections

--- QA/pi_client.py
+++ QA/pi_client.py
@@ -1,24 +1,23 @@
 import random
+import requests
 import socket
 
 HOST = socket.gethostbyname('elvisrodriguez.pythonanywhere.com')
 
-PORT = 80
+PORT = 443
 
 def generate_hb():
     hb = random.randint(80,160)
     yield hb
 
 client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-print(HOST)
-client_socket.settimeout(10)
 client_socket.connect((HOST, PORT))
-client_socket.settimeout(None)
 print('Connected to server at {host}'.format(host=HOST))
 hb = generate_hb()
-hb = str(next(hb))
-hb = hb.encode('utf-8')
-client_socket.sendall(hb)
+message = next(hb)
+print('Sending {message}'.format(message=message))
+message = message.encode('utf-8')
+client_socket.sendall(message)
 data = client_socket.recv(1024)
 print('Received {data}'.format(data=data))
 client_socket.close()
